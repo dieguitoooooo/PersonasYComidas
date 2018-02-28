58752966e0185b5149d214118f4d447679215ab6
diegoantonioalmontezarzuela@gmail.com <diegoantonioalmontezarzuela@gmail.com>
2018-02-28 11:01:34 +0100

    Añadido los metodos contestar y AlimentosMasCaloricoConsumido

--- Persona.java
+++ Persona.java
@@ -15,6 +15,7 @@ public class Persona
     private int edad;
     private int totalCalorias;
     private int metabolismoBasal;
+    private Comida comidaMasCaloricaIngerida;
 
     /**
      * Constructor para los objectos de la clase Persona
@@ -28,6 +29,7 @@ public class Persona
         this.altura = altura;
         this.edad = edad;
         this.totalCalorias = 0;
+        comidaMasCaloricaIngerida = null;
         //Si el sexo es falso esto quiere decir que es hombre.
         if(sexo=false){
             metabolismoBasal = (10*peso) + (6*altura) + (5*edad)+5;
@@ -48,17 +50,29 @@ public class Persona
      */
     public int comer(Comida comer)
     {
-        {
-            int caloriasComidaHastaElMomento = -1;
+        int caloriasComidaHastaElMomento = -1;
 
-            if(totalCalorias <= metabolismoBasal){
-                caloriasComidaHastaElMomento = comer.getCalorias();
-                totalCalorias += caloriasComidaHastaElMomento;
+        if(totalCalorias <= metabolismoBasal){
+            //come
+            caloriasComidaHastaElMomento = comer.getCalorias();
+            totalCalorias += caloriasComidaHastaElMomento;
+            if(comidaMasCaloricaIngerida !=null){
+                //ya habia comido cosas
+                if(comer.getCalorias() >= comidaMasCaloricaIngerida.getCalorias()){
+                    comidaMasCaloricaIngerida = comer;
+                }
             }
-            return caloriasComidaHastaElMomento;
+            else{
+                //No habia comido nada todavia
+                comidaMasCaloricaIngerida = comer;
+            }
         }
+        else{
+            //No come
+            System.out.println("Estoy Muy lleno, no puedo comer");
+        }
+        return caloriasComidaHastaElMomento;
     }
-
 
     /**
      * Metodo Que devuelve la cantidad de calorias ingeridas hasta el momento
@@ -69,6 +83,43 @@ public class Persona
     {
         return totalCalorias;
     }
-    
-    
+
+    /**
+     * Nos permite preguntarle cosas a la persona
+     * @param pregunta lo que le queremos preguntar
+     */
+    public String contestar(String pregunta)
+    {
+        String respuesta = "";
+        if(totalCalorias > metabolismoBasal || pregunta.toLowerCase().contains(nombre.toLowerCase())){
+            respuesta = pregunta.toUpperCase(); 
+        }
+        else{
+            if(pregunta.length() % 3 == 0){
+                respuesta = "SI";
+            }
+            else{
+                respuesta = "NO";
+            }
+        }
+        System.out.println(respuesta);
+        return respuesta;
+    }
+
+    /**
+     * Devuelve el nombre de la comida mas calorica consumida por la persona
+     * @return El nombre del alimento más calorico consumido hasta ahora
+     */
+    public String getAlimentosMasCaloricoConsumido()
+    {
+        String nombreComidaMasCalorica = null;
+        if(comidaMasCaloricaIngerida == null){
+            System.out.println("no he comido nada");
+        }
+        else{
+            nombreComidaMasCalorica = comidaMasCaloricaIngerida.getNombre();
+        }
+        return nombreComidaMasCalorica;
+    }
 }   
+
